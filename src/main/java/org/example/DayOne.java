eac3e4f12172d8b526f261c2d0d9cf60c4363190
Fredrik Stetler <Fredrik.Stetler@knightec.se>
2023-12-05 16:08:14 +0100

    Changed strings with regex

--- src/main/java/org/example/DayOne.java
+++ src/main/java/org/example/DayOne.java
@@ -19,7 +19,7 @@ public class DayOne {
                 }
             }
 
-            for (int j = listOfStrings.get(i).length() - 1; j > 0; j--) {
+            for (int j = listOfStrings.get(i).length() - 1; j >= 0; j--) {
                 if (Character.isDigit(listOfStrings.get(i).charAt(j))) {
                     stringNumber += listOfStrings.get(i).charAt(j);
                     break;
@@ -47,7 +47,39 @@ public class DayOne {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (int i = 0; i < wordList.size(); i++) {
+
+            if (wordList.get(i).contains("one")) {
+                wordList.set(i, wordList.get(i).replace("one", "1"));
+            }
+            if (wordList.get(i).contains("two")) {
+                wordList.set(i, wordList.get(i).replace("two", "2"));
+            }
+            if (wordList.get(i).contains("three")) {
+                wordList.set(i, wordList.get(i).replace("three", "3"));
+            }
+            if (wordList.get(i).contains("four")) {
+                wordList.set(i, wordList.get(i).replace("four", "4"));
+            }
+            if (wordList.get(i).contains("five")) {
+                wordList.set(i, wordList.get(i).replace("five", "5"));
+            }
+            if (wordList.get(i).contains("six")) {
+                wordList.set(i, wordList.get(i).replace("six", "6"));
+            }
+            if (wordList.get(i).contains("seven")) {
+                wordList.set(i, wordList.get(i).replace("seven", "7"));
+            }
+            if (wordList.get(i).contains("eight")) {
+                wordList.set(i, wordList.get(i).replace("eight", "8"));
+            }
+            if (wordList.get(i).contains("nine")) {
+                wordList.set(i, wordList.get(i).replace("nine", "9"));
+            }
+        }
+
+        System.out.println(wordList.get(12));
         return wordList;
     }
-
 }
