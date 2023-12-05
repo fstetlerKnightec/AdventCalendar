05b622a13977733ad4767693cf4484c10a48abf8
Fredrik Stetler <Fredrik.Stetler@knightec.se>
2023-12-05 15:18:01 +0100

    Added day one and dayone.txt

--- src/main/java/org/example/DayOne.java
+++ src/main/java/org/example/DayOne.java
@@ -1,4 +1,53 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DayOne {
+
+    public Integer calculateSumOfAllValues(List<String> listOfStrings) {
+        int totalValue = 0;
+        for (int i = 0; i < listOfStrings.size(); i++) {
+            String stringNumber = "";
+            for (int j = 0; j < listOfStrings.get(i).charAt(j); j++) {
+                if (Character.isDigit(listOfStrings.get(i).charAt(j))) {
+                    stringNumber += listOfStrings.get(i).charAt(j);
+                    break;
+                }
+            }
+
+            for (int j = listOfStrings.get(i).length() - 1; j > 0; j--) {
+                if (Character.isDigit(listOfStrings.get(i).charAt(j))) {
+                    stringNumber += listOfStrings.get(i).charAt(j);
+                    break;
+                }
+            }
+            totalValue += Integer.parseInt(stringNumber);
+        }
+        return totalValue;
+    }
+
+    public List<String> readFileAndReturnList(String filePath) {
+
+        ArrayList<String> wordList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    word = word.trim();
+                    if (!word.isEmpty()) {
+                        wordList.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordList;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,11 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> listOfStrings = new ArrayList<>(Arrays.asList(
-                "1abc2",
-                "pqr3stu8vwx",
-                "a1b2c3d4e5f",
-                "treb7uchet"));
-
-        System.out.println(calculateSumOfAllValues(listOfStrings));
-
+        DayOne dayOne = new DayOne();
+        List<String> listOfStrings = dayOne.readFileAndReturnList("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayOne.txt");
+        System.out.println(dayOne.calculateSumOfAllValues(listOfStrings));
     }
-
-    public static Integer calculateSumOfAllValues(List<String> listOfStrings) {
-        int totalValue = 0;
-        for (int i = 0; i < listOfStrings.size(); i++) {
-            String stringNumber = "";
-            for (int j = 0; j < listOfStrings.get(i).charAt(j); j++) {
-                if (Character.isDigit(listOfStrings.get(i).charAt(j))) {
-                    stringNumber += listOfStrings.get(i).charAt(j);
-                    break;
-                }
-            }
-
-            for (int j = listOfStrings.get(i).length() - 1; j > 0; j--) {
-                if (Character.isDigit(listOfStrings.get(i).charAt(j))) {
-                    stringNumber += listOfStrings.get(i).charAt(j);
-                    break;
-                }
-            }
-            totalValue += Integer.parseInt(stringNumber);
-        }
-        return totalValue;
-    }
-
 }
