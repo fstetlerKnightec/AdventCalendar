45035f9ccf85fedd45ed86ef87e2bba90d0a51a7
Fredrik Stetler <Fredrik.Stetler@knightec.se>
2023-12-07 12:13:28 +0100

    Fixed daytwo and made it work

--- src/main/java/org/example/DayTwo.java
+++ src/main/java/org/example/DayTwo.java
@@ -1,8 +1,88 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class DayTwo {
 
+    public static Integer ALLOWED_RED = 12;
+    public static Integer ALLOWED_GREEN = 13;
+    public static Integer ALLOWED_BLUE = 14;
+
+    List<String> listOfGames = readSentencesFromFile("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayTwo.txt");
 
 
+    public Integer numberOfAllAddedGames() {
+        int totalGameNumber = 0;
+        for (int i = 0; i < listOfGames.size(); i++) {
 
+            if (isAllowedGame(listOfGames.get(i))) {
+                totalGameNumber += (i+1);
+            }
+
+        }
+        return totalGameNumber;
+    }
+
+
+    public boolean isAllowedGame(String currentGame) {
+
+        int numberOfHandsAllowed = 0;
+
+        currentGame = currentGame + ";";
+        System.out.println(currentGame);
+        int totalRedPerGame = 0;
+        int totalBluePerGame = 0;
+        int totalGreenPerGame = 0;
+
+        boolean allowedGame = true;
+
+        for (int i = 0; i < currentGame.length(); i++) {
+
+            if (currentGame.startsWith("green", i)) {
+                totalGreenPerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 2)));
+                if (!String.valueOf(currentGame.charAt(i - 3)).isBlank()) {
+                    totalGreenPerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 3))) * 10;
+                }
+            }
+            if (currentGame.startsWith("red", i)) {
+                totalRedPerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 2)));
+                if (!String.valueOf(currentGame.charAt(i - 3)).isBlank()) {
+                    totalRedPerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 3))) * 10;
+                }
+            }
+            if (currentGame.startsWith("blue", i)) {
+                totalBluePerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 2)));
+                if (!String.valueOf(currentGame.charAt(i - 3)).isBlank()) {
+                    totalBluePerGame += Integer.parseInt(String.valueOf(currentGame.charAt(i - 3))) * 10;
+                }
+            }
+
+            if (String.valueOf(currentGame.charAt(i)).equals(";")) {
+                if (totalGreenPerGame <= ALLOWED_GREEN && totalBluePerGame <= ALLOWED_BLUE && totalRedPerGame <= ALLOWED_RED) {
+                    numberOfHandsAllowed += 1;
+                } else {
+                    allowedGame = false;
+                }
+                totalRedPerGame = 0;
+                totalBluePerGame = 0;
+                totalGreenPerGame = 0;
+            }
+        }
+        return allowedGame;
+    }
+
+    private static List<String> readSentencesFromFile(String filePath) {
+        List<String> sentences = null;
+        try {
+            Path path = Paths.get(filePath);
+            sentences = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentences;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
 package org.example;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        DayOne dayOne = new DayOne();
-        List<String> listOfStrings = dayOne.readFileAndReturnList("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayOne.txt");
-        System.out.println(dayOne.returnFirstNumberFromLeft(listOfStrings));
-        System.out.println(dayOne.returnFirstNumberFromRight(listOfStrings));
-        System.out.println(dayOne.addBothNumbersFromList(listOfStrings));
-        System.out.println(dayOne.addAllNumbersTogether(listOfStrings));
+        DayTwo dayTwo = new DayTwo();
 
+
+        System.out.println(dayTwo.numberOfAllAddedGames());
 
     }
 }
