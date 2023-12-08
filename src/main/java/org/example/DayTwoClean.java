bf78f16aab063ef7971957af721cc5db8926cbe4
Fredrik Stetler <Fredrik.Stetler@knightec.se>
2023-12-08 14:23:36 +0100

    Fixed part one of day two

---
+++ src/main/java/org/example/DayTwoClean.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DayTwoClean {
+
+    public final static Integer ALLOWED_RED = 12;
+    public final static Integer ALLOWED_GREEN = 13;
+    public final static Integer ALLOWED_BLUE = 14;
+
+    public Integer sumOfAllIDsFromAllowedGames(List<String> listOfGames) {
+
+        int totalSumOfIndexFromAllowedGames = 0;
+
+        for (int i = 0; i < listOfGames.size(); i++) {
+            if (isAllowedGame(listOfGames.get(i))) {
+                totalSumOfIndexFromAllowedGames += i+1;
+            }
+        }
+        return totalSumOfIndexFromAllowedGames;
+    }
+
+    public static boolean isAllowedGame(String currentGame) {
+        int numberOfGreenBalls = 0;
+        int numberOfBlueBalls = 0;
+        int numberOfRedBalls = 0;
+
+        boolean allowedGame = true;
+
+        for (int i = 0; i < currentGame.length(); i++) {
+            if (String.valueOf(currentGame.charAt(i)).equals(";")) {
+                if (numberOfGreenBalls > ALLOWED_GREEN || numberOfBlueBalls > ALLOWED_BLUE || numberOfRedBalls > ALLOWED_RED) {
+                    allowedGame = false;
+                }
+                numberOfGreenBalls = 0;
+                numberOfRedBalls = 0;
+                numberOfBlueBalls = 0;
+            }
+
+            numberOfGreenBalls += numberOfBallsPerHand(currentGame, "green", i);
+            numberOfBlueBalls += numberOfBallsPerHand(currentGame, "blue", i);
+            numberOfRedBalls += numberOfBallsPerHand(currentGame, "red", i);
+        }
+
+        return allowedGame;
+    }
+
+    public List<String> readGamesFromFileAndPutInList(String filePath) throws FileNotFoundException {
+        List<String> sentences;
+        try {
+            Path path = Paths.get(filePath);
+            sentences = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new FileNotFoundException("Could not find a file under path " + filePath);
+        }
+
+        List<String> sentecesWithAddedSemicolon = sentences.stream().map(s -> s + ";").toList();
+
+        return sentecesWithAddedSemicolon;
+    }
+
+    public static Integer numberOfBallsPerHand(String currentGame, String color, int index) { //Make an enum for color
+        int totalBallsPerHand = 0;
+        if (currentGame.startsWith(color, index)) {
+            totalBallsPerHand += Integer.parseInt(String.valueOf(currentGame.charAt(index - 2)));
+            if (!String.valueOf(currentGame.charAt(index - 3)).isBlank()) {
+                totalBallsPerHand += Integer.parseInt(String.valueOf(currentGame.charAt(index - 3))) * 10;
+            }
+        }
+
+        return totalBallsPerHand;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,21 +7,31 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        DayOneClean dayOneClean = new DayOneClean();
-        List<String> listOfStrings = dayOneClean.readFileAndReturnList("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayOne.txt");
+        DayTwoClean dayTwoClean = new DayTwoClean();
 
-        List<String> listOfFirstNumbersFromLeft = listOfStrings.stream()
-                .map(s -> dayOneClean.returnFirstNumberFromString(String.valueOf(s), false, true)).toList();
-        List<String> listOfFirstNumbersFromRight = listOfStrings.stream()
-                .map(s -> dayOneClean.returnFirstNumberFromString(String.valueOf(s), true, true)).toList();
+        List<String> listOfGames = dayTwoClean.readGamesFromFileAndPutInList("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayTwo.txt");
+        System.out.println(listOfGames.get(5));
+        System.out.println(dayTwoClean.isAllowedGame(listOfGames.get(5)));
+
+        System.out.println(dayTwoClean.sumOfAllIDsFromAllowedGames(listOfGames));
 
 
-        List<String> listOfCombinedNumbersFromLeftAndRight =
-                IntStream.range(0, listOfFirstNumbersFromLeft.size())
-                        .mapToObj(i -> listOfFirstNumbersFromLeft.get(i) + listOfFirstNumbersFromRight.get(i)).toList();
 
-        int totalSum = dayOneClean.addAllNumbersTogether(listOfCombinedNumbersFromLeftAndRight);
-        System.out.println(totalSum);
+//        DayOneClean dayOneClean = new DayOneClean();
+//        List<String> listOfStrings = dayOneClean.readFileAndReturnList("C:\\Programming\\Java\\AdventCalendar\\src\\main\\resources\\dayOne.txt");
+//
+//        List<String> listOfFirstNumbersFromLeft = listOfStrings.stream()
+//                .map(s -> dayOneClean.returnFirstNumberFromString(String.valueOf(s), false, true)).toList();
+//        List<String> listOfFirstNumbersFromRight = listOfStrings.stream()
+//                .map(s -> dayOneClean.returnFirstNumberFromString(String.valueOf(s), true, true)).toList();
+//
+//
+//        List<String> listOfCombinedNumbersFromLeftAndRight =
+//                IntStream.range(0, listOfFirstNumbersFromLeft.size())
+//                        .mapToObj(i -> listOfFirstNumbersFromLeft.get(i) + listOfFirstNumbersFromRight.get(i)).toList();
+//
+//        int totalSum = dayOneClean.addAllNumbersTogether(listOfCombinedNumbersFromLeftAndRight);
+//        System.out.println(totalSum);
 
     }
 }
