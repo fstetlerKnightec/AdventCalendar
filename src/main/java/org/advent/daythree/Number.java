7d7ebbdd9562c0ad7963192ad4323187fac68f95
frste <fredrik.stetler@knightec.se>
2023-12-14 08:44:33 +0100

    cleaned number class

--- src/main/java/org/advent/daythree/Number.java
+++ src/main/java/org/advent/daythree/Number.java
@@ -21,26 +21,6 @@ public class Number {
         this.hasAdjacentToSymbol = adjacentToSymbol;
     }
 
-    public boolean doesCharacterHaveAdjacantSymbol(List<String> listOfStrings, String symbol, int rowIndex, int columnIndex) {
-        return doesNumberHasAdjacantSymbol(listOfStrings, symbol, rowIndex, columnIndex, String.valueOf(numberValue).length());
-    }
-
-    public boolean doesOneDigitCharacterHasAdjacantSymbol(List<String> listOfStrings, String symbol, int rowIndex, int columnIndex) {
-
-//        for (Direction direction : Direction.values()) {
-//            System.out.println(direction.getCol());
-//        }
-
-        for (int rowOffset = -1; rowOffset < 2; rowOffset++) {
-            for (int colOffset = -1; colOffset < 2; colOffset++ ) {
-                if (String.valueOf(listOfStrings.get(rowIndex + rowIndex).charAt(columnIndex + colOffset)).equals(symbol)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
     public boolean doesNumberHasAdjacantSymbol(List<String> listOfStrings, String symbol, int rowIndex, int columnIndex, int numberLength) {
         for (int rowOffset = -1; rowOffset < 2; rowOffset++) {
             for (int colOffset = -1; colOffset < numberLength + 1; colOffset++ ) {
