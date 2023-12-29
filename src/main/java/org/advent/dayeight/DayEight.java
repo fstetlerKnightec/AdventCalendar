ea6a99e743a4541de9d1b210e3c7c9659a9da3f9
frste <fredrik.stetler@knightec.se>
2023-12-29 12:56:12 +0100

    read files

--- src/main/java/org/advent/dayeight/DayEight.java
+++ src/main/java/org/advent/dayeight/DayEight.java
@@ -1,9 +1,21 @@
 package org.advent.dayeight;
 
+import org.advent.Util;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class DayEight {
+    Util util = new Util();
 
-    public List<String>
+    public String rightLeftDirectionFromFile() throws IOException {
+        return util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString()).get(0);
+    }
+
+    public List<String> allLeftRightNodes() throws IOException {
+        List<String> allStrings = util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString());
+        return allStrings.subList(2, allStrings.size());
+    }
 
 }
