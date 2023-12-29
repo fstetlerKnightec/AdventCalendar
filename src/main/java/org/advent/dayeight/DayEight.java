ed67d502fd68cb525d206858eaaca28f686f0538
frste <fredrik.stetler@knightec.se>
2023-12-29 13:39:25 +0100

    util to status Util

--- src/main/java/org/advent/dayeight/DayEight.java
+++ src/main/java/org/advent/dayeight/DayEight.java
@@ -7,14 +7,13 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class DayEight {
-    Util util = new Util();
 
     public String rightLeftDirectionFromFile() throws IOException {
-        return util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString()).get(0);
+        return Util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString()).get(0);
     }
 
     public List<String> allLeftRightNodes() throws IOException {
-        List<String> allStrings = util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString());
+        List<String> allStrings = Util.readStringsFromFile(Paths.get("src/main/resources/dayEight.txt").toString());
         return allStrings.subList(2, allStrings.size());
     }
 
