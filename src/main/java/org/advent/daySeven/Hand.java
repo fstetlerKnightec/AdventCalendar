6be8adf964752a310466c5527ed581c37f72a1c8
frste <fredrik.stetler@knightec.se>
2024-01-05 10:44:49 +0100

    cleaned

--- src/main/java/org/advent/daySeven/Hand.java
+++ src/main/java/org/advent/daySeven/Hand.java
@@ -14,8 +14,6 @@ public class Hand {
     List<Character> restOfHand;
     int bid;
     Type type;
-    int rank;
-    boolean isPartTwo;
 
     public Hand(String fullString, boolean isPartTwo) {
         this.fullString = fullString;
@@ -32,46 +30,6 @@ public class Hand {
 
     public static Hand getHandFromString(String string, boolean isPartTwo) {
         return new Hand(string, isPartTwo);
-    }
-
-    public String handOfString(String string) {
-        return string.substring(0, string.indexOf(" "));
-    }
-
-    public List<Character> findNonRelevantCardsInHand() {
-        List<Character> listOfNonRelevantCards = new ArrayList<>(handString.chars().mapToObj(ch -> (char) ch).toList());
-        List<Character> listOfRelevantCards = stringOfRelevantCards();
-        for (Character character : listOfRelevantCards) {
-            listOfNonRelevantCards.remove(character);
-        }
-        return listOfNonRelevantCards;
-    }
-
-
-    public List<Character> stringOfRelevantCards() {
-        List<Character> listOfCards = new ArrayList<>();
-
-        Map<Character, Integer> mapOfLettersAndHowMany = findMultipleLetters(handString);
-
-        for (Character character : mapOfLettersAndHowMany.keySet()) {
-            for (int i = 0; i < mapOfLettersAndHowMany.get(character); i++) {
-                listOfCards.add(character);
-            }
-        }
-        return listOfCards;
-    }
-
-    private Map<Character, Integer> findMultipleLetters(String string) {
-        char[] charArray = string.toCharArray();
-
-        Map<Character, Integer> charCountMap = new HashMap<>();
-        for (char c : charArray) {
-            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
-        }
-        return charCountMap.entrySet().stream()
-                .filter(entry -> entry.getValue() > 1)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
     }
 
     public Type typeOfHand(boolean isPartTwo) {
@@ -96,11 +54,9 @@ public class Hand {
             }
         }
 
-
         if (allCardsMatch() && listIsSize(5)) {
             type = Type.FIVE_OF_A_KIND;
         }
-
 
         if (allCardsMatch() && listIsSize(4)) {
             type = Type.FOUR_OF_A_KIND;
@@ -112,7 +68,6 @@ public class Hand {
         }
 
 
-
         if (allCardsMatch() && listIsSize(3)) {
             type =  Type.THREE_OF_A_KIND;
             if (isPartTwo) {
@@ -122,8 +77,6 @@ public class Hand {
             }
         }
 
-
-
         if (allCardsMatch() && listIsSize(2)) {
             type =  Type.ONE_PAIR;
             if (isPartTwo) {
@@ -132,7 +85,6 @@ public class Hand {
                 }
             }
         }
-
 
         if (relevantCards.isEmpty()) {
             type =  Type.HIGH_CARD;
@@ -149,48 +101,6 @@ public class Hand {
         return relevantCards.stream().allMatch(ch -> ch.equals(relevantCards.get(0)));
     }
 
-    public boolean listIsSize(int size) {
-        return relevantCards.size() == size;
-    }
-
-
-    public String getFullString() {
-        return fullString;
-    }
-
-    public void setFullString(String fullString) {
-        this.fullString = fullString;
-    }
-
-    public void setHandString(String handString) {
-        this.handString = handString;
-    }
-
-    public List<Character> getRelevantCards() {
-        return relevantCards;
-    }
-
-    public void setRelevantCards(List<Character> relevantCards) {
-        this.relevantCards = relevantCards;
-    }
-
-    public List<Character> getRestOfHand() {
-        return restOfHand;
-    }
-
-    public void setRestOfHand(List<Character> restOfHand) {
-        this.restOfHand = restOfHand;
-    }
-
-    public boolean isPartTwo() {
-        return isPartTwo;
-    }
-
-    public void setPartTwo(boolean partTwo) {
-        this.isPartTwo = partTwo;
-    }
-
-
     public Type getType() {
         return type;
     }
@@ -203,19 +113,48 @@ public class Hand {
         return bid;
     }
 
-    public void setBid(int bid) {
-        this.bid = bid;
-    }
-    public void setType(Type type) {
-        this.type = type;
+    private Map<Character, Integer> findMultipleLetters(String string) {
+        char[] charArray = string.toCharArray();
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for (char c : charArray) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+        return charCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
     }
 
-    public int getRank() {
-        return rank;
+    private List<Character> stringOfRelevantCards() {
+        List<Character> listOfCards = new ArrayList<>();
+
+        Map<Character, Integer> mapOfLettersAndHowMany = findMultipleLetters(handString);
+
+        for (Character character : mapOfLettersAndHowMany.keySet()) {
+            for (int i = 0; i < mapOfLettersAndHowMany.get(character); i++) {
+                listOfCards.add(character);
+            }
+        }
+        return listOfCards;
     }
 
-    public void setRank(int rank) {
-        this.rank = rank;
+    private boolean listIsSize(int size) {
+        return relevantCards.size() == size;
+    }
+
+
+    private String handOfString(String string) {
+        return string.substring(0, string.indexOf(" "));
+    }
+
+    private List<Character> findNonRelevantCardsInHand() {
+        List<Character> listOfNonRelevantCards = new ArrayList<>(handString.chars().mapToObj(ch -> (char) ch).toList());
+        List<Character> listOfRelevantCards = stringOfRelevantCards();
+        for (Character character : listOfRelevantCards) {
+            listOfNonRelevantCards.remove(character);
+        }
+        return listOfNonRelevantCards;
     }
 
 }
