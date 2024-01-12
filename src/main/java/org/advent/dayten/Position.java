0f2de0c0ac7e78d6cdbe1b53ea1cfc03de1b85f4
frste <fredrik.stetler@knightec.se>
2024-01-12 15:11:03 +0100

    saved

--- src/main/java/org/advent/dayten/Position.java
+++ src/main/java/org/advent/dayten/Position.java
@@ -11,29 +11,6 @@ public record Position(int xPosition, int yPosition, char character) {
     }
 
     public CoordinateDirection nextStep(Position previousPosition) {
-
-//        return CoordinateDirection.getDirection(character, previousPosition);
-
-        if (character == 'L') {
-            return CoordinateDirection.getDirection('L', previousPosition, xPosition, yPosition);
-        }
-
-        if (character == '-') {
-            return CoordinateDirection.getDirection('-', previousPosition, xPosition, yPosition);
-        }
-
-        if (character == '7') {
-            return CoordinateDirection.getDirection('7', previousPosition, xPosition, yPosition);
-        }
-
-        if (character == '|') {
-            return CoordinateDirection.getDirection('|', previousPosition, xPosition, yPosition);
-        }
-
-        if (character == 'J') {
-            return CoordinateDirection.getDirection('J', previousPosition, xPosition, yPosition);
-        }
-
-        return CoordinateDirection.getDirection('F', previousPosition, xPosition, yPosition);
+        return CoordinateDirection.getDirection(character, previousPosition, xPosition, yPosition);
     }
 }
